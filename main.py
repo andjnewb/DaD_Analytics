2cf8970b723856af38d655c2cda938ae8b822573
Andrew Newbill <fidolimp@gmail.com>
2024-01-21 17:55:46 -0600

    Got a sort of price scanner working.
    Main issue is that it assigns the price to all of the daggers, can't figure out why,

--- main.py
+++ main.py
@@ -3,30 +3,26 @@ from roboflow import Roboflow
 from dotenv import load_dotenv
 import pytesseract
 import numpy as np
-import cv2
 import os
-import json
 import time
 import pickle
+import re
 from desktopmagic.screengrab_win32 import (
 	getDisplayRects, saveScreenToBmp, saveRectToBmp, getScreenAsImage,
 	getRectAsImage, getDisplaysAsImages)
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 image_frames = 'image_frames'
 
 #Weapons strings 
-sword_names = ["Arming", "Falchion", "Longsword", "Rapier", "Short", "Viking", "Zweihander"]
+sword_names = ["Arming Sword", "Falchion", "Longsword", "Rapier", "Short Sword", "Viking Sword", "Zweihander"]
 mace_names = ["Flanged Mace", "Morning Star", "Torch", "Quarterstaff", "War Maul"]
-dagger_names = ["Castillon", "Kris", "Rondel", "Stiletto"]
+dagger_names = ["Castillon Dagger", "Kris Dagger", "Rondel Dagger", "Stiletto Dagger"]
 polearm_names = ["Bardiche", "Halberd", "Spear"]
-axe_names = ["Battle", "Double", "Felling", "Hatchet", "Horsemans"]
-bow_names = ["Longbow", "Recurve", "Survival"]
-crossbow_names = ["Crossbow", "Hand", "Windlass"]
-magic_stuff_names = ["Ball", "Sword", "Staff", "Spellbook"]
+axe_names = ["Battle Axe", "Double Axe", "Felling Axe", "Hatchet", "Horsemans Axe"]
+bow_names = ["Longbow", "Recurve Bow", "Survival Bow"]
+crossbow_names = ["Crossbow", "Hand Crossbow", "Windlass Crossbow"]
+magic_stuff_names = ["Crystal Ball", "Crystal Sword", "Magic Staff", "Spellbook"]
 instrument_names = ["Drum", "Flute", "Lute", "Lyre"]
-shield_names = ["Buckler", "Heater", "Pavise", "Round"]
-
+shield_names = ["Buckler", "Heater Shield", "Pavise", "Round Shield"]
 
 def captureDaDScreenshot(image_name):
     rects = getDisplayRects()
@@ -95,61 +91,80 @@ def checkForSwords(listing, sword_map):
     #create map for number of each sword
     
     for sword in sword_map:
-        sword_map[sword] += checkStringForSubtrings(listing, sword)
+        sword_map[sword] += checkStringForSubstrings(listing, sword)
 
 def checkForMaces(listing, mace_map):
     #create map for number of each mace
     
     for mace in mace_map:
-        mace_map[mace] += checkStringForSubtrings(listing, mace)    
+        mace_map[mace] += checkStringForSubstrings(listing, mace)    
 
 def checkForDaggers(listing, dagger_map):
     #create map for number of each dagger
     
     for dagger in dagger_map:
-        dagger_map[dagger] += checkStringForSubtrings(listing, dagger)
+        dagger_map[dagger] += checkStringForSubstrings(listing, dagger)
 
 def checkForPolearms(listing, polearm_map):
     #create map for number of each sword
     
     for polearm in polearm_map:
-        polearm_map[polearm] += checkStringForSubtrings(listing, polearm)
+        polearm_map[polearm] += checkStringForSubstrings(listing, polearm)
 
 def checkForAxes(listing, axe_map):
     #create map for number of each sword
     
     for axe in axe_map:
-        axe_map[axe] += checkStringForSubtrings(listing, axe)
+        axe_map[axe] += checkStringForSubstrings(listing, axe)
 
 def checkForBows(listing, bow_map):
     #create map for number of each sword
     
     for bow in bow_map:
-        bow_map[bow] += checkStringForSubtrings(listing, bow)
+        bow_map[bow] += checkStringForSubstrings(listing, bow)
 
 def checkForCrossbows(listing, crossbow_map):
     #create map for number of each sword
     
     for crossbow in crossbow_map:
-        crossbow_map[crossbow] += checkStringForSubtrings(listing, crossbow)
+        crossbow_map[crossbow] += checkStringForSubstrings(listing, crossbow)
 
 def checkForMagicStuff(listing, magicstuff_map):
     #create map for number of each sword
     
     for magicitem in magicstuff_map:
-        magicstuff_map[magicitem] += checkStringForSubtrings(listing, magicitem)
+        magicstuff_map[magicitem] += checkStringForSubstrings(listing, magicitem)
 
 def checkForInstruments(listing, instrument_map):
     #create map for number of each sword
     
     for instrument in instrument_map:
-        instrument_map[instrument] += checkStringForSubtrings(listing, instrument)
+        instrument_map[instrument] += checkStringForSubstrings(listing, instrument)
 
 def checkForShields(listing, shield_map):
     #create map for number of each sword
     
     for shield in shield_map:
-        shield_map[shield] += checkStringForSubtrings(listing, shield)
+        shield_map[shield] += checkStringForSubstrings(listing, shield)
+
+def checkForPrices(listing, price_map):
+
+    for weapon in dagger_names:
+        nameReg = r"\[" + weapon + r"\]"
+        nameFound = re.search("[" + weapon + "]", listing)
+
+        if(nameFound != None):
+            print("Found a name")
+            priceFound = re.search(r"[0-9]+.g", listing)
+
+            if(priceFound != None):
+                print("Found a price")
+                #Get rid of anything that isn't a number in the price we found
+                cleaned = ''.join(filter(str.isdigit, priceFound.group()))
+                price_map[weapon] = int(cleaned)
+
+
+
 
 def writeWeaponDataToFile(weapon_maps):
 
@@ -195,24 +210,11 @@ def initWeaponMaps():
     
     return [sword_map, mace_map, dagger_map, polearm_map, axe_map, bow_map, crossbow_map, magicstuff_map, instrument_map, shield_map]
 
-def checkStringForSubtrings(string, substring):
+def checkStringForSubstrings(string, substring):
     return string.count(substring)
 
-
-if __name__ == '__main__':
-    quit = 0
-    image_index = 0
-    weapon_maps = initWeaponMaps()
-    while(quit == 0):
-        time.sleep(1)
-        captureDaDScreenshot("image_" + str(image_index))
-        
-        
-        json1 = getJsonFromRoboflow("image_" + str(image_index))
-        clips = cropSellerListings(json1, "image_" + str(image_index) + ".bmp")
-        listings = convertClipsToStrings(clips)
-
-        for listing in listings:
+def checkWeaponListingsForVolume(listings):
+    for listing in listings:
             checkForSwords(listing, weapon_maps[0])
             checkForMaces(listing, weapon_maps[1])
             checkForDaggers(listing, weapon_maps[2])
@@ -223,9 +225,33 @@ if __name__ == '__main__':
             checkForMagicStuff(listing, weapon_maps[7])
             checkForInstruments(listing, weapon_maps[8])
             checkForShields(listing, weapon_maps[9])
-        print(weapon_maps)
-        writeWeaponDataToFile(weapon_maps)
-        image_index += 1
+
+
+
+if __name__ == '__main__':
+    quit = 0
+    image_index = 0
+
+    #It may seem confusing to initialize volume maps and price maps with the same function, but both price and volume would be the same thing initially, zero.
+    #DRY or something
+    weapon_maps = initWeaponMaps()
+    price_maps = initWeaponMaps()
+    while(quit == 0):
+        #time.sleep(1)
+        #captureDaDScreenshot("image_" + str(image_index))
+        
+        
+        #json1 = getJsonFromRoboflow("image_" + str(image_index))
+        #clips = cropSellerListings(json1, "image_" + str(image_index) + ".bmp")
+        #listings = convertClipsToStrings(clips)
+
+        #checkWeaponListingsForVolume(listings)
+        print(price_maps[0])
+        checkForPrices("[12:02:09 AM]fantapp: [Kriss Dagger] 200g", price_maps[0])
+        print(price_maps)
+        exit()
+        #writeWeaponDataToFile(weapon_maps)
+        #image_index += 1
         
 
 
